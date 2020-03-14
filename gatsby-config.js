0e5a29c6ada5bce3b8442ef3b479e5ac1d0a9dd5
David Shugert <shugertdavid@gmail.com>
2020-03-14 12:06:28 -0600

    Added Google Analytics to website

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,12 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-159486532-1D",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
