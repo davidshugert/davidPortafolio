ae82b972e27b56cc4f14374ae7b17a46a7271c12
David Shugert <shugertdavid@gmail.com>
2020-03-25 12:25:08 -0600

    changed analytics  data

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-159486532-1D",
+        trackingId: "UA-159486532-1",
       },
     },
     {
